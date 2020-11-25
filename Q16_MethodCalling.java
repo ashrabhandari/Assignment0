5eee7224fe352753f0b850389db975b9c8b95401
Ashra Bhandari <bhandariashra@gmail.com>
2020-11-24 18:34:17 -0700

    modified the file

--- Q16_MethodCalling.java
+++ Q16_MethodCalling.java
@@ -8,7 +8,7 @@ public class Q16_MethodCalling {
 		System.out.println("Enter a number");
 		int n = input.nextInt();
 		
-		if (n % 2 == 0  ) {
+		if (n % 2 != 0  ) {
 			System.out.println(" This number is an even number");
 		}else {
 				System.out.println("This number is an odd number");
